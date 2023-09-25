7aff535272027a82fb27fb881e03b2a755ef7505
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-09-25 15:00:14 +0600

    3-2704._To_Be_Or_Not_To_Be.js

---
+++ 3-2704._To_Be_Or_Not_To_Be.js
@@ -0,0 +1,23 @@
+/*
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    function toBe(val1){
+        if(val1===val) return true;
+        throw new Error ("Not Equal");
+    };
+    function notToBe(val1){
+        if(val1!==val) return true;
+        throw new Error ("Equal");
+    }
+    return{
+        toBe,
+        notToBe
+    }
+};
+
+/*
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
