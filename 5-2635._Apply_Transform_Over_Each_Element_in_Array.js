fe636761033af1250d7191d9a54ae77378c3de7d
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-09-30 23:50:17 +0600

    5-2635._Apply_Transform_Over_Each_Element_in_Array.js

---
+++ 5-2635._Apply_Transform_Over_Each_Element_in_Array.js
@@ -0,0 +1,16 @@
+/*
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    for(let i=0; i<arr.length; i++)
+    {
+        arr[i] = fn(arr[i], i);
+    }
+    return arr;
+
+    // for(i in arr)
+    //     arr[i] = fn(arr[i], Number(i));
+    // return arr;
+};
