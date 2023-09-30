e9cf136ccff74d27257a9f697c6ab9772bd38543
Sajib Chandra Roy <93840340+sajibroy16@users.noreply.github.com>
2023-09-30 23:29:30 +0600

    4-2665._Counter_II.js

---
+++ 4-2665._Counter_II.js
@@ -0,0 +1,23 @@
+/*
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let i=0;
+    return {
+        increment:() => init + ++i,
+        decrement:() => init + --i,
+        reset: () =>{
+            i=0;
+            return init;
+        }
+
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
